0c79f610e0e6a3f2ca2ca8f7187a1da8a34f7950
Arkadyuti <bandyopadhyayarkadyuti@gmail.com>
2020-08-01 02:15:01 +0530

    Implemented time travelling feature

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from './Board';
 import { calculateWinner } from '../../helpers';
 
@@ -8,35 +8,45 @@ const style = {
 }
 
 function Game() {
-    const [board, setBoard] = useState(Array(9).fill(null));
+    const [history, setHistory] = useState(Array(9).fill(null));
+    const [stepNumber, setStepNumber] = useState(0);
     const [xIsNext, setXIsNext] = useState(true);
-    const winner = calculateWinner(board);
+    const winner = calculateWinner(history[stepNumber]);
 
 
     const handleClick = () => {
-        const boardCopy = [...board];
-        if (winner  || boardCopy[i]) {
+        const timeInHistory = history.slice(0, stepNumber+1);
+        const current = timeInHistory[stepNumber];
+        const squares = [...current];
+        if (winner  || squares[i]) {
             return;
         }
-        boardCopy[i] = xIsNext ? 'X' : 'O';
-        setBoard(boardCopy);
+        squares[i] = xIsNext ? 'X' : 'O';
+        setHistory([...timeInHistory, squares]);
+        setStepNumber(timeInHistory.length);
         setXIsNext(!xIsNext);
 
     }
 
-    const jumpTo = () => {
-
+    const jumpTo = (step) => {
+        setStepNumber(step);
+        setXIsNext(step % 2 === 0);
     }
 
     const renderMoves = () => (
-         <button onClick={() => setBoard(Array(9).fill(null))}>
-            Start Game
-         </button>
+          history.map((_step, move) => {
+          const destination = move ? `Go to moves#${move}` : 'Go to start';
+          return (
+              <li key={move}>
+                  <button onClick={() => jumpTo(move)}>{destination}</button>
+              </li>
+          )
+          })
     )
 
     return(
         <>
-        <Board squares={board} onClick={handleClick}/>
+        <Board squares={history[stepNumber]} onClick={handleClick}/>
         <div style={styles}>
             <p>{winner ? 'Winner: ' + winner : 'Next Player: ' + (xIsNext ? 'X' : 'O') }</p>
             {renderMoves()}
